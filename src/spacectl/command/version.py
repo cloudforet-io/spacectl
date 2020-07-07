e92ac2299e2574dff2a97b0cdfe3158ff66eee39
dongyo <dkang@mz.co.kr>
2020-07-07 14:55:04 +0900

    CLI update when --version updated

--- src/spacectl/command/version.py
+++ src/spacectl/command/version.py
@@ -2,8 +2,11 @@ import click
 import pkg_resources
 from spacectl.conf.global_conf import *
 
-__all__ = ['cli']
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ASCII_LOGO = os.path.join(BASE_DIR, 'template', 'ascii_logo')
+LOGO_BASIC = ['#####         ', '         ######', '####################################################']
 
+__all__ = ['cli']
 
 @click.group()
 def cli():
@@ -16,18 +19,32 @@ def version():
     click.echo(_get_version_from_pkg() or _get_version_from_file() or 'unknown')
 
 
+def _get_ascii_logo():
+    version_str = ''
+    f = open(ASCII_LOGO, 'r')
+    lines = f.readlines()
+    for line in lines:
+        version_str = version_str+line
+    f.close()
+    return version_str
+
 def _get_version_from_pkg():
     try:
-        return pkg_resources.get_distribution('spacectl').version
+        _version = pkg_resources.get_distribution('spacectl').version
+        version_info = LOGO_BASIC[0] + 'spacectl version: ' + _version + LOGO_BASIC[1] + '\n' + LOGO_BASIC[2]
+        return version_info
     except Exception:
         return None
 
 
 def _get_version_from_file():
+
     try:
         with open(os.path.join(SRC_DIR, 'VERSION'), 'r') as f:
             _version = f.read().strip()
             f.close()
-            return _version
+            if(_version is not None):
+                version_info = LOGO_BASIC[0] + 'spacectl version: ' +  _version + LOGO_BASIC[1] + '\n' + LOGO_BASIC[2]
+                return _get_ascii_logo() + version_info
     except Exception:
         return None
