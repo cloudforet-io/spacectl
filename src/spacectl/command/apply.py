48572e39315ef4a5cf6f132203729a92e0408d3e
Jinsu Park <bo314@naver.com>
2020-08-28 12:11:43 +0900

    Update: apply can be main

--- src/spacectl/command/apply.py
+++ src/spacectl/command/apply.py
@@ -18,20 +18,22 @@ def cli():
 
 @cli.command()
 @click.argument('file_path')
-#@click.option('-f', '--file-path', 'file_paths', multiple=True, type=click.Path(exists=True), help='manifest yaml file to apply. Multiple files will be overridden by order.')
 @click.option('-o', '--output', default='yaml', help='Output format',
               type=click.Choice(['json', 'yaml', 'none']), show_default=True)
 @click.option('-e', '--env', 'env', multiple=True, help='Configure additional environment variables.')
 @click.option('-v', '--var', 'var', multiple=True, help='Configure additional variables. You can override existing variables as well.')
 @click.option('--var-file', help='Input yaml file to configure var and env')
-# @click.option('--no-progress', is_flag=True, default=False, show_default=True)
 @click.option('-s', '--silent', is_flag=True, default=False, show_default=True)
 def apply(file_path, output, env, var, var_file, silent):
+    store.initialize(env, var, var_file)
+
     task_manager = TaskManager(silent)
     task_manager.load(file_path)
-    task_manager.set_input(env, var, var_file)
-
     task_manager.run()
 
     if not output == 'none':
         print_data(store.get_output(), output)
+
+
+if __name__ == '__main__':
+    apply()
