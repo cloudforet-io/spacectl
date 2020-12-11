3e45862e5fec4df3937e18984d41dc8b59cbc8fe
Jongmin Kim <whdalsrnt@megazone.com>
2020-12-11 14:24:52 +0900

    Bug fix: fix a version command error
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spacectl/command/version.py
+++ src/spacectl/command/version.py
@@ -1,12 +1,8 @@
+import os
 import click
 import pkg_resources
 from spacectl.conf.global_conf import *
-
-ASCII_LOGO = os.path.join(ASSET_DIR, 'ascii_logo')
-LOGO_BASIC = ['#####         '
-              , '         ######',
-              '\n',
-              '####################################################']
+from spacectl.lib.output import print_data
 
 __all__ = ['cli']
 
@@ -19,29 +15,13 @@ def cli():
 @cli.command()
 def version():
     """Print the client version information"""
-    click.echo(_get_version_from_pkg() or _get_version_from_file() or 'unknown')
-
-
-def _get_ascii_logo():
-    version_str = ''
-    f = open(ASCII_LOGO, 'r')
-    lines = f.readlines()
-    for line in lines:
-        version_str = version_str+line
-    f.close()
-    return version_str + '\n'
-
-
-def _get_version_info(_version):
-    LOGO_BASIC.insert(0, _get_ascii_logo())
-    LOGO_BASIC.insert(2, 'spacectl version: ' + _version)
-    return ''.join(LOGO_BASIC)
+    _version = _get_version_from_pkg() or _get_version_from_file() or 'unknown'
+    print_data({'version': _version}, 'yaml')
 
 
 def _get_version_from_pkg():
     try:
-        _version = pkg_resources.get_distribution('spacectl').version
-        return _get_version_info(_version)
+        return pkg_resources.get_distribution('spacectl').version
     except Exception:
         return None
 
@@ -49,8 +29,7 @@ def _get_version_from_pkg():
 def _get_version_from_file():
     try:
         with open(os.path.join(SRC_DIR, 'VERSION'), 'r') as f:
-            _version = f.read().strip()
-            if _version:
-                return _get_version_info(_version)
+            return f.read().strip()
+
     except Exception:
         return None
