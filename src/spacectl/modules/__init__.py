2966dfbd2cc593b1970c37de918031856f4d2a73
Jongmin Kim <whdalsrnt@megazone.com>
2022-08-30 22:35:46 +0900

    feat: add export_s3 module
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>
    (cherry picked from commit 54af42768b016dee1e0cc83a3ce5bdb254284970)

--- src/spacectl/modules/__init__.py
+++ src/spacectl/modules/__init__.py
@@ -1,10 +1,11 @@
-from spacectl.modules import shell, print, resource, export_google_sheets, date, join, concat
+from spacectl.modules import shell, print, resource, export_google_sheets, date, join, concat, export_s3
 
 MODULES = {
     'shell': shell.Task,
     'print': print.Task,
     'resource': resource.Task,
     'export-google-sheets': export_google_sheets.Task,
+    'export-s3': export_s3.Task,
     'date': date.Task,
     'join': join.Task,
     'concat': concat.Task
